d529e7fc0b9bc331fc1672995cb98411034addc8
Paul Whitmer <paul.whitmer@gmail.com>
2018-02-25 11:38:54 -0800

    installed redux-thunk

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,16 +1,17 @@
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import loginReducer from './reducers/loginReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     login: loginReducer
 } as {});
 
-// const middleware: Array<{}> = [];
+const middleware = [thunk];
 
 export default function configureStore() {
     return createStore(
         rootReducer,
-        composeWithDevTools(applyMiddleware())
+        composeWithDevTools(applyMiddleware(...middleware))
     );
 }
